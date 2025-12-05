587ce787d5368d1efa5f7241d679301952045c01
S.Nagachaitanyababu <nagachaitanyababusiga@gmail.com>
2025-12-05 21:20:40 +0530

    adding all the files

--- Closures.swift
+++ Closures.swift
@@ -45,3 +45,59 @@ print(newlst)
 newlst = students.filter({ $0.name >= "H" })
 
 print(newlst)
+
+let studentRankings = students.sorted{$0.name > $1.name}
+
+print(studentRankings)
+
+
+
+//closure
+
+let greet = {(name: String) -> String in
+     "Hello \(name) !!"
+}
+
+print(greet("Swift"))
+
+//call backs
+func fetch(complete: (String) -> Void ){
+    complete("Data loaded");
+}
+
+
+fetch{ string in 
+    print(string)
+}
+
+//completion Handlers
+
+func DownloadFile(url: String, completion : (Bool) -> Void){
+    completion(true);
+}
+
+var completion: (Bool) -> Void = { flag in 
+    print(flag ? "Download success":"Download failed");
+}
+
+DownloadFile(url: "File1.txt",completion: completion)
+
+//swift button actions
+/*
+Button("Click me"){
+    print("button tapped")
+}
+*/
+
+// Functional programming
+let numbers : Array<Int> = [1,2,3,4,5,6]
+
+let filtered = numbers.filter{$0%2==0}
+
+print("Filtered: \(filtered)")
+
+let mapped = numbers.map{$0*$0}
+print("Mapped: \(mapped)")
+
+let reduced = numbers.reduce(0){$0+$1}
+print("Sum is : \(reduced)")
