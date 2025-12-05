09946558e935aab2e3f430400515a71ecf9314b8
S.Nagachaitanyababu <nagachaitanyababusiga@gmail.com>
2025-12-05 10:22:08 +0530

    few changes complete the section

--- Optional.swift
+++ Optional.swift
@@ -43,4 +43,83 @@ struct me{
 }
 
 let msg=me(child: Child(child: grandChild(age: 10)))
-print(msg.child?.child?.age)
+// print(msg.child?.child?.age)
+
+//arrays
+
+var arr=["a","b","c","d"]
+
+/*
+for i in arr{
+    print("\(i)", terminator: " ")
+}
+*/
+
+for i in stride(from: 0, through: 100, by:2){
+    arr.append(String(Character(UnicodeScalar(i%26+97)!)));
+}
+
+print(arr)
+
+//dictonary
+
+let dict1 :[String: String] = ["a": "A","b": "B"]
+print(dict1)
+
+for (i,j) in dict1{
+    print("i: \(i), j: \(j)")
+}
+
+/*
+for i in dict1.keys{
+    print("i: \(i), j: \(dict1[i])")
+}
+*/
+
+print(arr.isEmpty)
+print(dict1.isEmpty)
+
+arr.sort()
+print(arr)
+
+let temp=dict1.sorted{$0.key>$1.key}
+print(temp)
+
+//struct and class
+struct Suser {
+    var user: Int
+}
+
+class Cuser{
+    var user: Int
+    init(_ user: Int){
+        self.user=user;
+    }
+}
+
+var suser: Suser = Suser(user: 1)
+var cuser: Cuser = Cuser(2)
+
+var a=suser
+a.user=3
+print(a)
+print(suser)
+
+var b=cuser
+b.user=4
+//print(b.user ?? 0)
+//print(cuser.user ?? 0)
+
+struct check{
+    var count: Int;
+    mutating func inc(){
+        self.count=self.count+1
+    }
+    func get()->Int{
+        return count
+    }
+}
+
+var ch=check(count: 0)
+ch.inc()
+print(ch.get())
